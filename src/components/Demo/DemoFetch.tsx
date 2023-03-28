f74b0e3c6f6c6dcef9ec9f63dda7d9e66ff26ba7
ppawel1 <ppawel1@yandex.ru>
2023-03-29 00:01:24 +0300

    components

---
+++ src/components/Demo/DemoFetch.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import useFetch from '../../hooks/useFetch'
+
+const Demo: React.FC = () => {
+  const { 
+    data, 
+    isLoading, 
+    error, 
+    refetch,
+  } = useFetch('https://jsonplaceholder.typicode.com/posts')
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => refetch({
+          params: {
+            _limit: 3,
+            // _offset: 10
+          }
+        })}>
+          Перезапросить
+        </button>
+      </div>
+
+      {isLoading && 'Загрузка...'}
+
+      {error && 'Произошла ошибка'}
+
+      {data && !isLoading && data.map((item: any) => <div key={item.id}>{item.title}</div>) }
+    </div>
+  )
+}
+
+export default Demo;
