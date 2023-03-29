3c333cc54d3b7f327129cffbd36738d6e991bb3a
ppawel1 <ppawel1@yandex.ru>
2023-03-29 23:40:55 +0300

    demo window scroll

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 // import DemoFetch from './components/demo/DemoFetch';
 // import DemoLocalStorage from './components/demo/DemoLocalStorage';
 // import DemoHover from './components/demo/DemoHover';
-import DemoViewport from './components/demo/DemoViewport';
+// import DemoViewport from './components/demo/DemoViewport';
+import DemoWindowScroll from './components/demo/DemoWindowScroll';
 
 const App: React.FC = () => {
   return (
     // <DemoFetch />
     // <DemoLocalStorage />
-    <DemoViewport />
+    // <DemoViewport />
+    <DemoWindowScroll />
   );
 }
 
---
+++ src/hooks/useWindowScroll.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import useWindowEvent from './useWindowEvent';
+
+
+interface ScrollPosition {
+  x: number;
+  y: number;
+}
+
+const getScrollPosition = (): ScrollPosition => {
+  return typeof window !== 'undefined'
+    ? { x: window.pageXOffset, y: window.pageYOffset }
+    : { x: 0, y: 0 };
+}
+
+const scrollTo = ({ x, y }: Partial<ScrollPosition>) => {
+  if (typeof window !== 'undefined') {
+    const scrollOptions: ScrollToOptions = { behavior: 'smooth' };
+
+    if (typeof x === 'number') {
+      scrollOptions.left = x;
+    }
+
+    if (typeof y === 'number') {
+      scrollOptions.top = y;
+    }
+
+    window.scrollTo(scrollOptions);
+  }
+}
+
+const useWindowScroll = () => {
+  const [position, setPosition] = useState<ScrollPosition>({ x: 0, y: 0 });
+
+  useWindowEvent('scroll', () => setPosition(getScrollPosition()));
+  useWindowEvent('resize', () => setPosition(getScrollPosition()));
+
+  useEffect(() => {
+    setPosition(getScrollPosition());
+  }, []);
+
+  return [position, scrollTo] as const;
+}
+
+export default useWindowScroll;
